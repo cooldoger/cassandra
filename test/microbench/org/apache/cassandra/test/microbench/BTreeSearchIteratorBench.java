e80f0bb4e4398acf13e47f298483f3ad2db0bb62
Jay Zhuang <jay.zhuang@yahoo.com>
2017-01-20 17:22:55 -0800

    Add more jmh test

--- test/microbench/org/apache/cassandra/test/microbench/BTreeSearchIteratorBench.java
+++ test/microbench/org/apache/cassandra/test/microbench/BTreeSearchIteratorBench.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 import org.apache.cassandra.utils.btree.BTreeSearchIterator;
 import org.apache.cassandra.utils.btree.BTree;
@@ -42,15 +43,17 @@ import org.openjdk.jmh.annotations.Warmup;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 50, time = 1, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 1)
+@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 20, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 2)
 @Threads(1)
 @State(Scope.Benchmark)
 public class BTreeSearchIteratorBench
 {
     private int sum;
-    private Object[] btree;
+    private Object[] btreeBig;  // 1000 nodes
+    private Object[] btreeLeaf; // 32 nodes leaf
+    private Object[] btreeOneElem; // only one node
 
     private static List<Integer> seq(int count)
     {
@@ -76,14 +79,103 @@ public class BTreeSearchIteratorBench
     @Setup(Level.Trial)
     public void setup() throws Throwable
     {
-        btree = BTree.build(seq(1000), UpdateFunction.noOp());
+        btreeBig = BTree.build(seq(1000, 0, 2), UpdateFunction.noOp());
+        btreeLeaf = BTree.build(seq(32, 0, 2), UpdateFunction.noOp());
+        btreeOneElem = BTree.build(seq(1), UpdateFunction.noOp());
+        sum = 0;
     }
 
     @Benchmark
     public void searchBigTreeTest()
     {
-        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btree, CMP, Dir.ASC);
-        Integer val = iter.next(546);
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeBig, CMP, Dir.ASC);
+        Random rand = new Random(2);
+        Integer val = iter.next(rand.nextInt(1000) * 2);
+        assert(val != null);
         sum += val;
+    }
+
+    @Benchmark
+    public void searchNotFoundBigTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeBig, CMP, Dir.ASC);
+        Random rand = new Random(2);
+        Integer val = iter.next(rand.nextInt(1000) * 2 + 1);
+        assert(val == null);
+        if (val != null) {
+            throw new RuntimeException("Should not find any result:" + val.toString());
+        }
+    }
+
+    @Benchmark
+    public void iteratorBigTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeBig, CMP, Dir.ASC);
+        while(iter.hasNext())
+        {
+            sum += iter.next();
+        }
+    }
+
+    @Benchmark
+    public void searchFoundLeafTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeLeaf, CMP, Dir.ASC);
+        Random rand = new Random(2);
+        Integer val = iter.next(rand.nextInt(32) * 2);
+        assert(val != null);
+        sum += val;
+    }
+
+    @Benchmark
+    public void searchNotFoundLeafTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeLeaf, CMP, Dir.ASC);
+        Random rand = new Random(2);
+        Integer val = iter.next(rand.nextInt(32) * 2 + 1);
+        assert(val == null);
+        if (val != null) {
+            throw new RuntimeException("Should not find any result: " + val.toString());
+        }
+    }
+
+    @Benchmark
+    public void iteratorLeafTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeLeaf, CMP, Dir.ASC);
+        while(iter.hasNext())
+        {
+            sum += iter.next();
+        }
+    }
+
+    @Benchmark
+    public void searchFoundOneElemTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeOneElem, CMP, Dir.ASC);
+        Integer val = iter.next(0);
+        assert(val != null);
+        sum += val;
+    }
+
+    @Benchmark
+    public void searchNotFoundOneElemTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeOneElem, CMP, Dir.ASC);
+        Integer val = iter.next(1);
+        assert(val == null);
+        if (val != null) {
+            throw new RuntimeException("Should not find any result: " + val.toString());
+        }
+    }
+
+    @Benchmark
+    public void iteratorOneElemTreeTest()
+    {
+        BTreeSearchIterator<Integer, Integer> iter = BTree.slice(btreeOneElem, CMP, Dir.ASC);
+        while(iter.hasNext())
+        {
+            sum += iter.next();
+        }
     }
 }
