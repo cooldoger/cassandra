06d28001ef1619c22918e4e4879387a914040d43
Jay Zhuang <jay.zhuang@yahoo.com>
2017-04-19 12:16:42 -0700

    LWT support for cassandra-stress
    Patch by Jay Zhuang; reviewed by ** for CASSANDRA-7960

--- tools/stress/src/org/apache/cassandra/stress/StressProfile.java
+++ tools/stress/src/org/apache/cassandra/stress/StressProfile.java
@@ -543,6 +543,10 @@ public class StressProfile implements Serializable
 
                         //Put PK predicates at the end
                         sb.append(pred);
+                        if (insert.containsKey("condition")) {
+                            sb.append(" " + insert.get("condition"));
+                            insert.remove("condition");
+                        }
                     }
                     else
                     {
